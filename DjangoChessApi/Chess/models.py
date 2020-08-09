6075ef5e8ce853454ad0d89966d114d2f649bcda
Theo Voss <theo.voss973@gmail.com>
2020-08-09 00:10:17 -0400

    Update to django JSONFIELD definition since the other one is apparently deprecated according to terminal warnings on server startup.

--- DjangoChessApi/Chess/models.py
+++ DjangoChessApi/Chess/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.postgres.fields import JSONField
 from django.contrib.auth.models import User
 from django.db.models.deletion import SET_NULL, CASCADE
 
@@ -10,13 +9,13 @@ from chess.chess_configurations import get_standard_chess_pieces
 class GameType(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
-    rules = JSONField(default=get_standard_chess_pieces)
+    rules = models.JSONField(default=get_standard_chess_pieces)
     created_by = models.ForeignKey(User, null=True, on_delete=SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Game(models.Model):
-    data = JSONField() # is the data used by the chess library
+    data = models.JSONField() # is the data used by the chess library
 
     @property
     def rules(self):
@@ -34,7 +33,7 @@ class Game(models.Model):
     def turn_color(self):
         return self.data['players'][self.turn]['color']
 
-    history = JSONField(null=True)
+    history = models.JSONField(null=True)
     player1 = models.ForeignKey(User, null=True, on_delete=CASCADE, related_name='games_player1')
     player2 = models.ForeignKey(User, null=True, on_delete=CASCADE, related_name='games_player2')
 
