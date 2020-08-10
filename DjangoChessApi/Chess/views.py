a7365899c0401124f88dabd098cf03058d5cbd49
Theo Voss <theo.voss973@gmail.com>
2020-08-10 11:32:15 -0400

    Add header and place at top of every page. Reorganize templates a bit.

--- DjangoChessApi/Chess/views.py
+++ DjangoChessApi/Chess/views.py
@@ -25,12 +25,12 @@ def current_datetime(request):
 
 def home(request):
     game_types = GameType.objects.all()
-    return render(request, 'chess/home.html', {'game_types': game_types})
+    return render(request, 'chess/main/home.html', {'game_types': game_types})
 
 
 def create_game(request):
     game_types = GameType.objects.all()
-    return render(request, 'chess/create_game.html', {'game_types': game_types})
+    return render(request, 'chess/main/create_game.html', {'game_types': game_types})
 
 
 def create_game_redirect(request, game_type_id):
@@ -47,7 +47,7 @@ def play_game(request, game_id):
     displayable_board = get_displayable_board(game.board)
     destinations_url = reverse('move-destinations', args=[game.id])
     move_url = reverse('move-move', args=[game.id])
-    return render(request, 'chess/play_game.html', {
+    return render(request, 'chess/main/play_game.html', {
         'name': name,
         'board': displayable_board,
         'destinations_url': destinations_url,
@@ -63,7 +63,7 @@ def create_configuration(request):
             return HttpResponseRedirect(reverse('configure', game_type.id))
     else:
         form = GameTypeForm()
-    return render(request, 'chess/create_configuration.html', {'form': form})
+    return render(request, 'chess/main/create_configuration.html', {'form': form})
 
 
 def configuration(request, game_type_id):
@@ -95,9 +95,11 @@ def configuration(request, game_type_id):
         'id': game_type.id,
         'checkmark_url': checkmark_url,
         'pieces': pieces,
-        'directions': directions,
-        'movements': movement_rules,
-        'capture_actions': capture_actions,
+        'enums': {
+            'directions': directions,
+            'movements': movement_rules,
+            'capture_actions': capture_actions
+        },
         'form': form
     }
-    return render(request, 'chess/piece_configurations.html', context)
+    return render(request, 'chess/main/configuration.html', context)
