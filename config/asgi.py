be5af988b07cb94831033b33e521cf3c30fa2cf2
Theo Voss <theo.voss973@gmail.com>
2020-12-21 00:13:35 -0500

    update dependencies

--- config/asgi.py
+++ config/asgi.py
@@ -1,21 +1,18 @@
 import os
-import django
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 import DjangoChessApi.Chess.routing
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
-django.setup()
-application = get_default_application()
 
-
-# application = ProtocolTypeRouter({
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter(
-#             DjangoChessApi.Chess.routing.websocket_urlpatterns
-#         )
-#     ),
-#     "http": get_asgi_application(),
-# })
+application = ProtocolTypeRouter({
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            DjangoChessApi.Chess.routing.websocket_urlpatterns
+        )
+    ),
+    "http": get_asgi_application(),
+})
