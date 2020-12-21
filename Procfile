64838e032c812f2ea0c14a0303c6e49e00d4e673
Theo Voss <theo.voss973@gmail.com>
2020-12-20 23:47:51 -0500

    Switch to uvicorn for websockets

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn config.wsgi --log-file -
+web: uvicorn config.asgi:application --port=$PORT
 release: python manage.py migrate && python manage.py cleandata
--- config/asgi.py
+++ config/asgi.py
@@ -1,17 +1,21 @@
 import os
+import django
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 import DjangoChessApi.Chess.routing
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
-application = ProtocolTypeRouter({
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            DjangoChessApi.Chess.routing.websocket_urlpatterns
-        )
-    ),
-    "http": get_asgi_application(),
-})
+django.setup()
+application = get_default_application()
+
+
+# application = ProtocolTypeRouter({
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             DjangoChessApi.Chess.routing.websocket_urlpatterns
+#         )
+#     ),
+#     "http": get_asgi_application(),
+# })
