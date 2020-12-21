2211ff5c807b5028507930e4fc3620961b192ab0
Theo Voss <theo.voss973@gmail.com>
2020-12-21 00:22:54 -0500

    django 1.9 upgrade

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn config.asgi --log-file -
+web: uvicorn config.asgi --log-file -
 release: python manage.py migrate && python manage.py cleandata
--- config/asgi.py
+++ config/asgi.py
@@ -3,10 +3,12 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import DjangoChessApi.Chess.routing
+import django
 
+
+django.setup()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
-
 
 application = ProtocolTypeRouter({
     "websocket": AuthMiddlewareStack(
