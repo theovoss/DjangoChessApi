2ad6fb7fa6b055486dd6f15f3474a166c5f7a3fa
Theo Voss <theo.voss973@gmail.com>
2020-12-22 12:50:51 -0500

    Update asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -1,17 +1,19 @@
 import os
 import django
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+django.setup()
+application = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from DjangoChessApi.Chess.routing import websocket_urlpatterns
 
 
 application = ProtocolTypeRouter({
     # Django's ASGI application to handle traditional HTTP requests
-    "http": get_asgi_application(),
+    "http": application,
 
     # WebSocket chat handler
     "websocket": AuthMiddlewareStack(
