942124d12dc991346c7e772325065e2dec4fdf35
Theo Voss <theo.voss973@gmail.com>
2020-12-21 22:27:53 -0500

    Fix DJANGO_SETTINGS_MODULE settings issue

--- config/asgi.py
+++ config/asgi.py
@@ -1,12 +1,12 @@
 import os
 import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from DjangoChessApi.Chess.routing import websocket_urlpatterns
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dropdjango.settings")
 
 
 application = ProtocolTypeRouter({
