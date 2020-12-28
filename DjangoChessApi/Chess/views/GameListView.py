2ee062858d1f1a2b59f02c6928d7f7245b882be5
Theo Voss <theo.voss973@gmail.com>
2020-12-28 12:00:04 -0500

    specify template heroku can't find but local can

--- DjangoChessApi/Chess/views/GameListView.py
+++ DjangoChessApi/Chess/views/GameListView.py
@@ -7,6 +7,7 @@ class GameListView(ListView):
 
     context_object_name = 'my_games'
     paginate_by = 100  # if pagination is desired
+    template_name = "chess/game_list.html"
 
     def get_queryset(self):
         return Game.objects.filter(player1=self.request.user) | Game.objects.filter(
